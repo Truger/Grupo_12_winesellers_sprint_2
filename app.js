94b5b2bbbc8a7a25d3c50ac2727556a1a843297f
Truger <ezequieljeroncic@hotmail.com>
2021-04-28 20:08:33 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const port= process.env.PORT;
+const path = require("path");
+const publicPatch = path.resolve(__dirname, "./public");
+app.use(express.static("Public"));
+
+app.get("/", (req, res) => { 
+    res.sendFile(path.join(__dirname, "/views/home.html")) 
+});
+
+app.get("/register", (req, res) => {
+     res.sendFile(path.join(__dirname, "/views/register.html")) 
+    });
+
+app.get("/cart", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/cart.html")) 
+});
+
+app.get("/login", (req, res) => {
+     res.sendFile(path.join(__dirname, "/views/login.html")) 
+    });
+
+app.get("/productDetails", (req, res) => {
+     res.sendFile(path.join(__dirname, "/views/productDetails.html")) 
+    });  
+
+app.listen(port|| 3001, () => {
+    console.log('Listening on port'+port);
+});
